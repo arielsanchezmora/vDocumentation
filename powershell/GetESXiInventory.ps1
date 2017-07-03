b22cdd0a4402a78830ed2dda181118665ad470c2
Edgar Sanchez <ed.sanchez@live.com>
2017-07-03 12:08:45 -0500

    PowerShell

--- powershell/GetESXiInventory.ps1
+++ powershell/GetESXiInventory.ps1
@@ -14,6 +14,7 @@
     Downloaded from: http://poshcode.org/?show=928
 .Link
   https://github.com/edmsanchez/vDocumentation
+  https://virtualcornerstone.com/vDocumentation/
 .INPUTS
    No inputs required
 .OUTPUTS
@@ -30,18 +31,32 @@
    The name(s) of the vSphere Virtual DataCenter(s)
 .EXAMPLE
     GetESXiInventory.ps1 -datacenter vDC001
+.PARAMETER ExportCSV
+    Exports all data to CSV file. File is saved on current userpath from where the script was executed
+.EXAMPLE
+    GetESXiInventory.ps1 -cluster production-cluster -ExportCSV
+.PARAMETER ExportExcel
+    Exports all data to Excel file (No need to have Excel Installed). This relies on ImportExcel Module to be installed.
+    ImportExcel Module can be installed directly from the Powershell Gallery. See https://github.com/dfinke/ImportExcel for more information
+    the  File is saved on current userpath from where the script was executed.
+.EXAMPLE
+    GetESXiInventory.ps1 -cluster production-cluster -ExportExcel
 #>
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 
-param(
+param (
     $esxi,
     $cluster,
-    $datacenter
+    $datacenter,
+    [switch]$ExportCSV,
+    [switch]$ExportExcel
 )
 
 $outputCollection = @()
-$outputFile = "Inventory.csv"
+$date = Get-Date -format s
+$date = $date -replace ":","-"
+$outputFile = "Inventory" + $date
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
@@ -95,85 +110,125 @@ function Get-VMHostWSManInstance {
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 # Check to see if there are any currently connected servers
-if($Global:DefaultViServers.Count -gt 0) {
+if ($Global:DefaultViServers.Count -gt 0) {
     Clear-Host
-    Write-Host -ForegroundColor Green "`tConnected to " $Global:DefaultViServers
+    Write-Host "`tConnected to " $Global:DefaultViServers -ForegroundColor Green
 } else {
-    Write-Host -ForegroundColor Red "`tError: You must be connected to a vCenter or a vSphere Host before running this script."
+    Write-Host "`tError: You must be connected to a vCenter or a vSphere Host before running this script." -ForegroundColor Red
     break
 }
 
 # Check to make sure at least 1 parameter was used
-if([string]::IsNullOrWhiteSpace($esxi) -and [string]::IsNullOrWhiteSpace($cluster) -and [string]::IsNullOrWhiteSpace($datacenter)) {
-    Write-Host -ForegroundColor Red "`tError: You must at least use one parameter, run Get-Help " $MyInvocation.MyCommand.Name " for more information"
+if ([string]::IsNullOrWhiteSpace($esxi) -and [string]::IsNullOrWhiteSpace($cluster) -and [string]::IsNullOrWhiteSpace($datacenter)) {
+    Write-Host "`tError: You must at least use one parameter, run Get-Help " $MyInvocation.MyCommand.Name " for more information" -ForegroundColor Red
     break
 }
 
 # Gather host list
-if([string]::IsNullOrWhiteSpace($esxi)) {
+if ([string]::IsNullOrWhiteSpace($esxi)) {
     # $Vmhost Parameter Empty
 
-    if([string]::IsNullOrWhiteSpace($cluster)) {
+    if ([string]::IsNullOrWhiteSpace($cluster)) {
         # $Cluster Parameter Empty
 
-        if([string]::IsNullOrWhiteSpace($datacenter)) {
+        if ([string]::IsNullOrWhiteSpace($datacenter)) {
             # $Datacenter Parameter Empty
 
         } else {                
             # Processing by Datacenter
-            Write-Host "`tGathering host list from the following DataCenter(s): " (@($datacenter) -join ',')
-            foreach ($vDCname in $datacenter) {
-                $tempList = Get-DataCenter $vDCname.Trim() | Get-VMHost 
-                $vHostList += $tempList | Sort-Object -Property name
+            if ($datacenter -eq "all vdc") {
+                Write-Host "`tGathering all hosts from the following vCenter(s): " $Global:DefaultViServers
+                $vHostList = Get-VMHost | Sort-Object -Property Name
+                
+            } else {
+                Write-Host "`tGathering host list from the following DataCenter(s): " (@($datacenter) -join ',')
+                foreach ($vDCname in $datacenter) {
+                    $tempList = Get-Datacenter -Name $vDCname.Trim() | Get-VMHost 
+                    $vHostList += $tempList | Sort-Object -Property Name
+                }
             }
         }
     } else {
         # Processing by Cluster
         Write-Host "`tGatehring host list from the following Cluster(s): " (@($cluster) -join ',')
         foreach ($vClusterName in $cluster) {
-            $tempList = Get-Cluster $vClusterName.Trim() | Get-VMHost 
-            $vHostList += $tempList | Sort-Object -Property name
+            $tempList = Get-Cluster -Name $vClusterName.Trim() | Get-VMHost 
+            $vHostList += $tempList | Sort-Object -Property Name
         }
     }
 } else {
     # Processing by ESXi Host
     Write-Host "`tGathering host list..."
-    foreach($invidualHost in $esxi) {
+    foreach ($invidualHost in $esxi) {
         $tempList = $invidualHost.Trim()
-        $vHostList += $tempList | Sort-Object -Property name
+        $vHostList += $tempList | Sort-Object -Property Name
     }
 }
 
 # Main code execution
-Foreach($esxihost in $vHostList) {
+Foreach ($esxihost in $vHostList) {
     $esxcli = Get-EsxCli -VMHost $esxihost
-    $vmhost = Get-VMHost $esxihost
-    Write-Host "`tGathering information from $vmhost ..."
-    $hardware = $vmhost | Get-VMHostHardware | Select Manufacturer, Model, CpuModel, CpuCount, CpuCoreCountTotal, MhzPerCpu, MemorySlotCount, NicCount  
-    $vmInfo = $vmhost | Select MemoryTotalGB, Version, Build
+    $vmhost = Get-VMHost -Name $esxihost
+    # Skip if ESXi host is not in Connected or Maintenance ConnectionState
+    if ($vmhost.ConnectionState -eq "Connected" -or $vmhost.ConnectionState -eq "Maintenance") {
+        # Do nothing - ESXi host is reachable
+    } else {
+        # Make a combined object
+        $skiphosts = New-Object -TypeName PSObject -Property ([ordered]@{
+            'Hostname' = $esxihost
+            'Connection State' = $esxihost.ConnectionState
+            })
+        #Add the object to the collection
+        $skipCollection += $skiphosts
+        continue
+    }
 
     # Get inventory info
-    $mgmtIP = $vmhost | Get-VMHostNetworkAdapter | Where-Object {$_.ManagementTrafficEnabled -eq 'True'} | Select -ExpandProperty IP
+    Write-Host "`tGathering information from $vmhost ..."
+    $hardware = $vmhost | Get-VMHostHardware -SkipAllSslCertificateChecks -WaitForAllData
+    $vmInfo = $vmhost | Select-Object -Property MemoryTotalGB, Build
+    $mgmtIP = $vmhost | Get-VMHostNetworkAdapter | Where-Object {$_.ManagementTrafficEnabled -eq 'True'} | Select-Object -ExpandProperty IP
     $hardwarePlatfrom = $esxcli.hardware.platform.get()
     $vmRam = $vmInfo.MemoryTotalGB -as [int]
-    $racIP = Get-VMHostWSManInstance -VMHost $vmhost -class OMC_IPMIIPProtocolEndpoint -ignoreCertFailures | Select -ExpandProperty IPv4Address
+
+    # Get RAC IP
+    # Try with -class OMC_IPMIIPProtocolEndpoint First
+    $rac = Get-VMHostWSManInstance -VMHost $vmhost -class OMC_IPMIIPProtocolEndpoint -ignoreCertFailures -ErrorAction SilentlyContinue
+    if ($rac.Name) {
+        $racIP = $rac.IPv4Address
+    } else { # Else try with -class CIM_IPProtocolEndpoint
+        $rac = Get-VMHostWSManInstance -VMHost $vmhost -class CIM_IPProtocolEndpoint -ignoreCertFailures -ErrorAction SilentlyContinue
+        if ($rac.Name) {
+            $racIP = $rac | Where-Object {$_.Name -match "Management Controller IP"} | Select-Object -ExpandProperty IPv4Address
+        }
+    }
+
+    #Get ESXi version details
+    $VMHostView = $vmhost | Get-View
+    $EsxiVersion = $esxcli.system.version.get()
     
     # Make a combined object
-    $inventoryResults = New-Object -Type PSObject -Prop ([ordered]@{
+    $inventoryResults = New-Object -TypeName PSObject -Property ([ordered]@{
         'Hostname' = $vmhost
         'Management IP' = $mgmtIP
         'RAC IP' = $racIP
-        'OS/ESXi' = $vmInfo.Version
-        'Build' = $vmInfo.Build
+        'Product' = $VMHostView.Config.Product.Name
+        'Version' = $VMHostView.Config.Product.Version
+        'Build' = $Vminfo.Build
+        'Update' = $EsxiVersion.Update
+        'Patch' = $EsxiVersion.Patch
         'Make'= $hardware.Manufacturer
         'Model' = $hardware.Model
         'S/N' = $hardwarePlatfrom.serialNumber
+        'BIOS Version' = $hardware.BiosVersion
         'CPU Model' = $hardware.CpuModel
         'CPU Count' = $hardware.CpuCount
         'CPU Core Total' = $hardware.CpuCoreCountTotal
         'Speed (MHz)' = $hardware.MhzPerCpu
         'Memory (GB)' = $vmRam
-        'Memory Slot Count' = $hardware.MemorySlotCount
+        'Memory Slots Count' = $hardware.MemorySlotCount
+        'Memory Slots Used' = $hardware.MemoryModules.Count
+        'Power Supplies' = $hardware.PowerSupplies.Count
         'NIC Count' = $hardware.NicCount
     })
     # Add the object to the collection
@@ -181,9 +236,22 @@ Foreach($esxihost in $vHostList) {
 }
 
 # Display output on screen
-Write-Host -ForegroundColor Green "`n" "ESXi Inventory:"
+Write-Host "`n" "ESXi Inventory:" -ForegroundColor Green
 $outputCollection | Format-List 
 
 # Export combined object
-Write-Host -ForegroundColor Green "`tData was saved to" $outputFile "CSV file"
-$outputCollection | Export-Csv $outputFile -NoTypeInformation
+if ($ExportCSV) {
+    Write-Host "`tData was saved to" ($outputFile + ".csv") "file" -ForegroundColor Green
+    $outputCollection | Export-Csv ($outputFile + ".csv") -NoTypeInformation
+}
+
+if ($ExportExcel) {
+    if (Get-Module -ListAvailable -Name ImportExcel) {
+        Write-Host "`tData was saved to" ($outputFile + ".xlsx") "file" -ForegroundColor Green
+        $outputCollection | Export-Excel ($outputFile + ".xlsx") -BoldTopRow -WorkSheetname Inventory
+    } else {
+        Write-Host "ImportExcel Module missing, see help for more information" -ForegroundColor Red
+    }
+}
+
+
